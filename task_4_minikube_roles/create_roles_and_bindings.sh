4568ebb411f7ac88686628ef7ecdbf2fac2cb0f6
Алексей Головизин <agolovizin@alfabank.ru>
2025-09-02 01:40:01 +0300

    Ага, роли таки нужно создавать для к конкретных namespace. Ну ладно = )

--- task_4_minikube_roles/create_roles_and_bindings.sh
+++ task_4_minikube_roles/create_roles_and_bindings.sh
@@ -6,3 +6,13 @@ kubectl apply -f $BASEDIR/clusterrolebinding-namespace-viewer.yaml
 
 kubectl apply -f $BASEDIR/clusterrole-cluster-viewer.yaml
 kubectl apply -f $BASEDIR/clusterrolebinding-cluster-viewer.yaml
+
+# Применяем роль sales-app-deployer в каждом sales namespace
+kubectl apply -f $BASEDIR/role-sales-app-deployer.yaml -n sales-services-prod
+kubectl apply -f $BASEDIR/role-sales-app-deployer.yaml -n sales-services-dev
+kubectl apply -f $BASEDIR/role-sales-app-deployer.yaml -n sales-services-test1
+kubectl apply -f $BASEDIR/role-sales-app-deployer.yaml -n sales-services-test2
+kubectl apply -f $BASEDIR/role-sales-app-deployer.yaml -n sales-services-test3
+
+# Применяем RoleBindings для sales-devops группы
+kubectl apply -f $BASEDIR/rolebinding-sales-app-deployer.yaml
---
+++ task_4_minikube_roles/users/create_example_users.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+
+bash $BASEDIR/create_user.sh bob namespace-viewer sales-devops
