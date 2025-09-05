109e43970e1156846fc3dab04872786876fe2c27
Алексей Головизин <agolovizin@alfabank.ru>
2025-09-05 12:20:58 +0300

    ---

--- task_6_audit_logs/logs_and_comments_2/open_audit_log.py
+++ task_6_audit_logs/logs_and_comments_2/open_audit_log.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 if __name__ == "__main__":
     df = pd.read_json(open("audit_5.log"), lines=True)
+    # Тут ставим Breakpoint, чтобы можно было открывать датафрейм в PyCharm как View As Dataframe --
+    # -- и смотреть как в Excel, с поиском и фильтрацией
     print(df)
